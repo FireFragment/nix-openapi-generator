d1e1afbdf46547e37876e021540fe3ea017f6634
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-05-12 13:45:38 +0200

    Remove `generators_old`

--- default.nix
+++ default.nix
@@ -83,15 +83,4 @@ rec {
             };
         };
     };
-
-    generators-old = {
-        "cpp-qt-client" = createGenerator {
-            generatorName = "cpp-qt-client";
-            binaryOverrides = { src, ... } : {
-                nativeBuildInputs = with pkgs; [ cmake libsForQt5.wrapQtAppsHook ];
-                buildInputs = with pkgs; [ libsForQt5.qtbase ];
-                src = "${src}/client";
-            };
-        };
-    };
 }

17cc780e9444674bf4de88ffe6c30ec6f00c5e88
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-05-12 16:58:27 +0200

    Better error message for haskell

--- generators/haskell-http-client/default.nix
+++ generators/haskell-http-client/default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
-    dontBuildBinaryReason = "Not yet implemented";
+    dontBuildBinaryReason = "Not yet implemented, use source code as the library for now.";
     binaryOverrides = { ... } : {
 
     };

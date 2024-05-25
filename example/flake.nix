7f40f00c7472b2b880da2b5decb9717b82034094
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-05-25 14:27:23 +0200

    Use `cpp-qt-client` in the example

--- example/flake.nix
+++ example/flake.nix
@@ -19,8 +19,8 @@
         pkgs = nixpkgs.legacyPackages.${system};
         openapi-gen = openapi-generator.lib.${system};
 
-        # Use the "c" generator
-        generated = (openapi-gen.getGenerator "c") {
+        # Use the "cpp-qt-client" generator
+        generated = (openapi-gen.getGenerator "cpp-qt-client") {
           specification = pkgs.fetchurl {
             url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/9df68a1dafd467d9fdbf68653f351b860b4ec6e5/examples/v3.0/petstore.yaml";
             sha256 = "sha256-WYE2y5BOF+ju6tUa4z3Y1AH9/0VdLXTzhpxKpfJ0ImY=";
